3e47aabdbdd0069b8b77c2577d7bc6d9d08974bf
Nickborovkov <borovckov@yandex.ru>
2021-08-05 16:56:31 +0300

    created preloader

---
+++ src/preloader.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './App.css'
+
+const Preloader = () => {
+    return (
+        <h1 className='preloader'>Loading, please wait...</h1>
+    )
+}
+
+export default Preloader
