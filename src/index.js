989b12caa9f52ff5ae6ae985f28f678a5100e9e5
Nikita Borovkov <borovckov@yandex.ru>
2021-08-07 23:35:03 +0300

    added BrowserRouter

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from "react-redux";
+import {BrowserRouter} from "react-router-dom";
 import store from "./reducers/store";
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App/>
-    </Provider>, document.getElementById('root')
+    <BrowserRouter>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </BrowserRouter>, document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
