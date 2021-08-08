838f3f054d5bcd89cef9a881b5e1803b5128e126
Nikita Borovkov <borovckov@yandex.ru>
2021-08-08 23:13:12 +0300

    added currentDate, intervalStartDate and intervalendDate values; added setCurrentDate and setInterval reducer cases and action creators

--- src/reducers/apod.js
+++ src/reducers/apod.js
@@ -1,10 +1,17 @@
 import {nasaRequest} from "../ajax/serverRequests";
+import {dateToday} from "../helpers/dateToday";
 
-const SET_APOD = `SET_APOD`
+const SET_APOD = `NASA/apod/SET_APOD`
+const SET_APOD_WITH_INTERVAL = `NASA/apod/SET_APOD_WITH_INTERVAL`
+const SET_CURRENT_DATE = `NASA/apod/SET_CURRENT_DATE`
+const SET_INTERVAL = `NASA/apod/SET_INTERVAL`
 
 
 const initialState = {
-    apodArray: []
+    apodArray: [],
+    currentDate: dateToday,
+    intervalDateStart: dateToday,
+    intervalDateEnd: dateToday,
 }
 
 
@@ -14,6 +21,22 @@ const apodReducer = (state = initialState, action) => {
             return {
                 ...state,
                 apodArray: [action.apodArray]
+            }
+        case SET_APOD_WITH_INTERVAL:
+            return {
+                ...state,
+                apodArray: action.apodArray
+            }
+        case SET_CURRENT_DATE:
+            return {
+                ...state,
+                currentDate: action.currentDate
+            }
+        case SET_INTERVAL:
+            return {
+                ...state,
+                intervalDateStart: action.start,
+                intervalDateEnd: action.end,
             }
         default:
             return state
@@ -28,9 +51,23 @@ export default apodReducer
 const setApod = (apodArray) =>
     ( {type: SET_APOD, apodArray} )
 
+const setApodWithInterval = (apodArray) =>
+    ( {type: SET_APOD_WITH_INTERVAL, apodArray} )
+
+export const setCurrentDate = (currentDate) =>
+    ( {type: SET_CURRENT_DATE, currentDate} )
+
+export const setIntervalDates = (start, end) =>
+    ( {type: SET_INTERVAL, start, end} )
+
 
 //THUNK
-export const getApod = async dispatch => {
-    const response = await nasaRequest.getAPOD()
+export const getApod = (date) => async dispatch => {
+    const response = await nasaRequest.getAPOD(date)
     dispatch(setApod(response.data))
+}
+
+export const getApodWithInterval = (startDate, endDate) => async dispatch => {
+    const response = await nasaRequest.getAPODwithInterval(startDate, endDate)
+    dispatch(setApodWithInterval(response.data))
 }
