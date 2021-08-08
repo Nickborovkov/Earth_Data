6a0c38ebf90d3cd6ea8efd79e8f2dc0cf24d1d3e
Nikita Borovkov <borovckov@yandex.ru>
2021-08-09 00:57:20 +0300

    temporary timezone fix

--- src/helpers/dateToday.js
+++ src/helpers/dateToday.js
@@ -1,3 +1,5 @@
 const now = new Date()
 
-export const dateToday = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`
+export const dateToday = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate() - 1}`
+//TODO fix timezones bug (when in Russia it's already next day, in USA where i suppose NASA server is situated is still previous day, so
+// the API don't work, cause USA is in a totally different timezone)
