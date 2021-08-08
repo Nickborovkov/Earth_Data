838de3d40d11143a7f5b3c84b27cb4089902afef
Nikita Borovkov <borovckov@yandex.ru>
2021-08-09 00:59:01 +0300

    added date picking, added correct rendering of astedoids array

--- src/components/neosw/neows.jsx
+++ src/components/neosw/neows.jsx
@@ -3,38 +3,52 @@ import s from './neows.module.css'
 import {useDispatch, useSelector} from "react-redux";
 import {getNeows} from "../../reducers/neows";
 import Preloader from "../../helpers/preloader";
+import SetIntervalNEOWS from "./datePickers/SetIntervalNEOWS";
 
 const Neows = () => {
 
     const dispatch = useDispatch()
     const neowsArray = useSelector(state => state.neows.neowsArray)
+    const intervalDateStart = useSelector(state => state.neows.intervalDateStart)
+    const intervalDateEnd = useSelector(state => state.neows.intervalDateEnd)
 
-    //const tempObj = Object.values(neowsArray[0])[0]
-    useEffect(()=>{
-        dispatch(getNeows)
-    },[])
 
-    if(neowsArray.length === 0) return <Preloader />
+    useEffect(() => {
+        dispatch(getNeows(intervalDateStart, intervalDateEnd))
+    }, [dispatch, intervalDateStart, intervalDateEnd])
 
+//TODO `1 week max`
+
+    if (neowsArray.length === 0) return <Preloader/>
 
     return (
         <div>
             <h2>Near Earth Object Web Service</h2>
+            <SetIntervalNEOWS />
+            {
+                <div>
+                    {
+                        Object.keys(neowsArray).map(key => <div key={key}>
+                            <h3 className={s.date}>Date: {key}</h3>
+                            {
+                                neowsArray[key].map(v => <div className={s.asteroid} key={v.id}>
+                                    <p className={s.name}>Asteroid name: {v.name}</p>
+                                    <div>
+                                        <p className={s.diameter}>Diameter</p>
+                                        <p>From {v.estimated_diameter.meters.estimated_diameter_min.toFixed(2)} meters</p>
+                                        <p>To {v.estimated_diameter.meters.estimated_diameter_max.toFixed(2)} meters</p>
+                                    </div>
+                                    <div>
+                                        <p className={s.danger}>Is potentially hazardous?</p>
+                                    </div>
+                                    <p>{!v.is_potentially_hazardous_asteroid ? `No` : `Yes`}</p>
 
-            <div>
-                {
-                    neowsArray.map(n => <div className={s.asteroid} key={n.id}>
-                        <h3>{n.name}</h3>
-                        <p>{n.id}</p>
-                        <p>{!n.is_potentially_hazardous_asteroid ? `Not hazardous` : `Potentially hazardous`}</p>
-                        <div>
-                            <h4>Estimated diameter</h4>
-                            <p>From {n.estimated_diameter.meters.estimated_diameter_min.toFixed(2)} meters</p>
-                            <p>To {n.estimated_diameter.meters.estimated_diameter_max.toFixed(2)} meters</p>
-                        </div>
-                    </div>)
-                }
-            </div>
+                                </div>)
+                            }
+                        </div>)
+                    }
+                </div>
+            }
         </div>
     )
 }
