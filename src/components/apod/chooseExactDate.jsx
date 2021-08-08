de2f533c760f0e231e53434afaad4242e2fb08ac
Nikita Borovkov <borovckov@yandex.ru>
2021-08-08 23:11:13 +0300

    added formik form to choose date

---
+++ src/components/apod/chooseExactDate.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import {Formik} from "formik";
+import * as yup from 'yup'
+import {useDispatch} from "react-redux";
+import {setCurrentDate} from "../../reducers/apod";
+import {dateToday} from "../../helpers/dateToday";
+
+const ChooseExactDate = () => {
+
+    const dispatch = useDispatch()
+
+    const validationSchema = yup.object().shape({
+        currentDate: yup.string().required(`Field is required`)
+    })
+
+    return (
+        <div>
+            <Formik
+                initialValues={{
+                    currentDate: ``
+                }}
+                validateOnBlur
+                onSubmit={ (values, {resetForm}) => {
+                    dispatch(setCurrentDate(values.currentDate))
+                    resetForm({values: ``})
+                } }
+                validationSchema={validationSchema}
+            >
+                {({values, errors, touched, handleChange, handleBlur, handleSubmit, isValid, dirty})=>(
+                    <div>
+                        <h3>Choose exact date</h3>
+                        {touched.currentDate && errors.currentDate &&
+                        <div>{errors.currentDate}</div>}
+                        <input type="date"
+                               name='currentDate'
+                               onChange={handleChange}
+                               onBlur={handleBlur}
+                               value={values.currentDate}
+                        />
+                        <button disabled={!isValid && !dirty}
+                                onClick={handleSubmit}
+                                type={`submit`}>
+                            Show</button>
+                    </div>
+                )}
+            </Formik>
+        </div>
+    )
+}
+
+export default ChooseExactDate
