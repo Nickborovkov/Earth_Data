0cf3fb09d6dc01099491ba3207990b3853ec4488
Nickborovkov <borovckov@yandex.ru>
2021-08-09 17:06:43 +0300

    update dateToday.jsx

--- src/helpers/dateToday.js
+++ src/helpers/dateToday.js
@@ -1,5 +1,11 @@
 const now = new Date()
 
-export const dateToday = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate() - 1}`
-//TODO fix timezones bug (when in Russia it's already next day, in USA where i suppose NASA server is situated is still previous day, so
-// the API don't work, cause USA is in a totally different timezone)
+export const yearNow = String(now.getFullYear())
+export const monthNow = String(now.getMonth() + 1).length === 1
+    ? `0${String(now.getMonth() + 1)}`
+    : String(now.getMonth() + 1)
+export const dayNow = String(now.getDate() - 1).length === 1
+    ? `0${String(now.getDate() - 1)}`
+    : String(now.getDate() - 1)
+
+export const dateToday = `${yearNow}-${monthNow}-${dayNow}`
