8def3d25f8174603450525565bfa85b805b4ecc4
Nickborovkov <borovckov@yandex.ru>
2021-08-09 17:10:36 +0300

    added started page

---
+++ src/helpers/jsxHelpersPages/startedPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const StartedPage = () => {
+    return (
+        <div>
+            <h1>This is an app build with NASA public API</h1>
+            <p>Some results may take some time to load due to long server response, please be patient</p>
+            <p>Made by Nick Borovkov</p>
+        </div>
+    )
+}
+
+export default StartedPage
