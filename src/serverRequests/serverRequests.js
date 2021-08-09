0937779cc184122e5a57d8ef9a018eed9b6aa5a0
Nickborovkov <borovckov@yandex.ru>
2021-08-09 17:11:36 +0300

    server requests updates

---
+++ src/serverRequests/serverRequests.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: `https://api.nasa.gov/`,
+})
+
+const apiKey = `ZWGiTATMzPHjpsUJSj289aerwaSsLpikIiYBhaek`
+
+export const nasaRequest = {
+    getAPOD (date) {
+        return instance.get(`planetary/apod?date=${date}&api_key=${apiKey}`)
+    },
+    getAPODwithInterval (startDate, endDate) {
+        return instance.get(`planetary/apod?start_date=${startDate}&end_date=${endDate}&api_key=${apiKey}`)
+    },
+    getNeows (startDate, endDate) {
+        return instance.get(`neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=${apiKey}`)
+    },
+    getEarthObservation (lon, lat, date, dim) {
+        return instance.get(`planetary/earth/assets?lon=${lon}&lat=${lat}&date=${date}&&dim=${dim}&api_key=${apiKey}`)
+    },
+    getEarthImage (date) {
+        return instance.get(`EPIC/api/natural/date/${date}?api_key=${apiKey}`)
+    },
+    getMarsRoverPhotos (rover, date) {
+        return instance.get(`mars-photos/api/v1/rovers/${rover}/photos?earth_date=${date}&page=1&api_key=${apiKey}`)
+    },
+    searchNasaLibrary () {
+        return instance.get(`search?q=moon&media_type=image`)
+    }
+}
