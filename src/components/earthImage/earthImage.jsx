d3f07ef572159e95dcff9e2d76b40d5fbb97cce3
Nickborovkov <borovckov@yandex.ru>
2021-08-09 17:19:48 +0300

    added date picking functionality

--- src/components/earthImage/earthImage.jsx
+++ src/components/earthImage/earthImage.jsx
@@ -3,25 +3,27 @@ import s from './earthImage.module.css'
 import {useDispatch, useSelector} from "react-redux";
 import {getEarthImage} from "../../reducers/earthImage";
 import Preloader from "../../helpers/preloader";
+import SetDateEarthImage from "./datePicker/setDateEarthImage";
+import {earthImageUrlHelper} from "../../helpers/earthImageURLHelper";
 
 const EarthImage = () => {
 
-    const earthImage = useSelector(state => state.earthImage.earthImage)
     const dispatch = useDispatch()
+    const earthImage = useSelector(state => state.earthImage.earthImage)
+    const SelectedDate = useSelector(state => state.earthImage.date)
 
     useEffect(() => {
-        dispatch(getEarthImage)
-    }, [])
+        dispatch(getEarthImage(SelectedDate))
+    }, [dispatch, SelectedDate])
 
-    let year = `2020`
-    let month = `08`
-    let day = `07`
+
 
     if(!earthImage) return <Preloader />
 
     return (
         <div>
             <h2>See dated Earth Image</h2>
+            <SetDateEarthImage />
             <div>
                 {
                     earthImage.map(e => <div key={e.identifier}>
@@ -30,8 +32,8 @@ const EarthImage = () => {
                             <p>Latitude {e.centroid_coordinates.lat}</p>
                             <p>Longitude {e.centroid_coordinates.lon}</p>
                         </div>
-                        <img className={s.image} src={`https://api.nasa.gov/EPIC/archive/natural/${year}/${month}/${day}/png/${e.image}.png?api_key=DEMO_KEY`}
-                             alt="earthImage"/>
+                        <img className={s.image} src={earthImageUrlHelper(SelectedDate, e.image)}
+                             alt="Not available, please change date"/>
                     </div>)
                 }
             </div>
--- src/reducers/earthImage.js
+++ src/reducers/earthImage.js
@@ -1,10 +1,13 @@
-import {nasaRequest} from "../ajax/serverRequests";
+import {nasaRequest} from "../serverRequests/serverRequests";
 
-const SET_EARTH_IMAGE = `SET_EARTH_IMAGE`
+const SET_EARTH_IMAGE = `NASA/earthImage/SET_EARTH_IMAGE`
+const SET_EARTH_IMAGE_DATE = `NASA/earthImage/SET_EARTH_IMAGE_DATE`
 
 
 const initialState = {
-    earthImage: undefined
+    earthImage: undefined,
+    //setting date when this date started to be collected, because today's date is not always available
+    date: `2015-06-13`,
 }
 
 
@@ -14,6 +17,11 @@ const earthImageReducer = (state = initialState, action) => {
             return {
                 ...state,
                 earthImage: action.earthImage
+            }
+        case SET_EARTH_IMAGE_DATE:
+            return {
+                ...state,
+                date: action.date
             }
         default:
             return state
@@ -27,10 +35,12 @@ export default earthImageReducer
 //AC
 const setEarthImage = (earthImage) =>
     ( { type: SET_EARTH_IMAGE, earthImage } )
+export const setEarthImageDate = (date) =>
+    ( { type: SET_EARTH_IMAGE_DATE, date } )
 
 
 //THUNK
-export const getEarthImage = async dispatch => {
-    const response = await nasaRequest.getEarthImage()
+export const getEarthImage = (date) => async dispatch => {
+    const response = await nasaRequest.getEarthImage(date)
     dispatch(setEarthImage(response.data))
 }
