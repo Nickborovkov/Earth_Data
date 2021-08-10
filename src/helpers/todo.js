c011af7074eb125f3a1cc3a6916e857a7df028f4
Nikita Borovkov <borovckov@yandex.ru>
2021-08-11 02:50:38 +0300

    update todo.js

--- src/helpers/todo.js
+++ src/helpers/todo.js
@@ -1,11 +1,14 @@
 //TODO `add search queries`
-//TODO `Fix search results`
+//TODO `restyle search results`
+//TODO `Add videos and audios in nasaLibrary after search`
 //TODO `make responsible`
 //TODO `fix timezone bug`
 //TODO `make function helper for formik, cause all forms are quite the same`
-//TODO `deploy at github pages`
 //TODO `fix manifest json (also add icon for mobile)`
-//TODO `add limits to forms`
+//TODO `add helper for forms and style forms`
+//TODO `in neows date show not correctly`
+//TODO `try to remove hashRouter with browserRouter`
+//TODO `add limits to forms or at least write limits in forms`
 //TODO `style forms errors`
 //TODO `add errors from server to forms`
 //TODO `add errors handling (try/catch)`
