b5d7063139392f1dea5c2289f2462261f46a5731
Nickborovkov <borovckov@yandex.ru>
2021-08-10 17:26:43 +0300

    added gh-pages,project deployed

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from "react-redux";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import store from "./reducers/store";
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>, document.getElementById('root')
+    </HashRouter>, document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
