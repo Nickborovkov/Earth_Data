7b24d80c7842ee0e8bd4ede52639481120984152
Nikita Borovkov <borovckov@yandex.ru>
2021-08-10 23:19:03 +0300

    redirect fix

--- src/helpers/jsxHelpersPages/page404.jsx
+++ src/helpers/jsxHelpersPages/page404.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import s from './helpers.module.css'
+import {useSelector} from "react-redux";
+import {Redirect} from "react-router-dom";
 
 const Page404 = () => {
+
+    const searchStart = useSelector(state => state.library.searchStart)
+
+    if(searchStart) return <Redirect to='/nasaLibrary'/>
+
     return (
         <div className={s.page404}>
             <h1 className={s.page404Title}>Error</h1>
--- src/helpers/jsxHelpersPages/startedPage.jsx
+++ src/helpers/jsxHelpersPages/startedPage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import s from './helpers.module.css'
+import {useSelector} from "react-redux";
+import {Redirect} from "react-router-dom";
 
 const StartedPage = () => {
+
+    const searchStart = useSelector(state => state.library.searchStart)
+
+    if(searchStart) return <Redirect to='/nasaLibrary'/>
+
     return (
         <div className={s.starterPage}>
             <h1 className={s.starterTitle}>This App shows Earth data collected by NASA</h1>
