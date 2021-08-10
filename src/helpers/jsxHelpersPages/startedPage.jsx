65cdaef868c8ea2aad0844e5e8640676d1aa9428
Nickborovkov <borovckov@yandex.ru>
2021-08-10 17:28:58 +0300

    starter page layout fix

--- src/helpers/jsxHelpersPages/startedPage.jsx
+++ src/helpers/jsxHelpersPages/startedPage.jsx
@@ -4,7 +4,7 @@ import s from './helpers.module.css'
 const StartedPage = () => {
     return (
         <div className={s.starterPage}>
-            <h1 className={s.starterTitle}>This is an app build with NASA public API</h1>
+            <h1 className={s.starterTitle}>This App shows Earth data collected by NASA</h1>
             <p className={s.starterSubtitle}>Results may take some time to load due to long server response, please be patient</p>
             <p className={s.starterSubtitle}>Made by Nick Borovkov</p>
         </div>
