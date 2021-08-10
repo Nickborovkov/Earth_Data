bec82cf7af4f7f84f01479bc6d2fac6dff810cb6
Nikita Borovkov <72877821+Nickborovkov@users.noreply.github.com>
2021-08-10 07:26:13 +0300

    Create todo.js

---
+++ src/helpers/todo.js
@@ -0,0 +1,16 @@
+//TODO `See what else to add from api (donki, insight)`
+//TODO `Move search to header`
+//TODO `add search queries`
+//TODO `Fix search results`
+//TODO `make search results appear`
+//TODO `style`
+//TODO `add icons`
+//TODO `make responsible`
+//TODO `fix timezone bug`
+//TODO `make function helper for formik, cause all forms are quite the same`
+//TODO `deploy at github pages`
+//TODO `fix manifest json (also add icon for mobile)`
+//TODO `add limits to forms`
+//TODO `style forms errors`
+//TODO `add errors from server to forms`
+//TODO `add errors handling (try/catch)`
