e319a27ec894a36fda1e47610fe3eebbaf512403
Nickborovkov <borovckov@yandex.ru>
2021-08-10 16:58:37 +0300

    update todo.js

--- src/helpers/todo.js
+++ src/helpers/todo.js
@@ -1,10 +1,5 @@
-//TODO `See what else to add from api (donki, insight)`
-//TODO `Move search to header`
 //TODO `add search queries`
 //TODO `Fix search results`
-//TODO `make search results appear`
-//TODO `style`
-//TODO `add icons`
 //TODO `make responsible`
 //TODO `fix timezone bug`
 //TODO `make function helper for formik, cause all forms are quite the same`
