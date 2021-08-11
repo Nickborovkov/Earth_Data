aa871532849c5ee1e860533c5cb50a2235c2cfa8
Nikita Borovkov <72877821+Nickborovkov@users.noreply.github.com>
2021-08-12 01:23:47 +0300

    Update todo.js

--- src/helpers/todo.js
+++ src/helpers/todo.js
@@ -6,3 +6,6 @@
 //TODO `try to remove hashRouter with browserRouter`
 //TODO `add limits to forms or at least write limits in forms`
 //TODO `add errors handling (try/catch)`
+//TODO `add home icon`
+//TODO `add list of dates for rovers`
+//TODO `fix titles`
