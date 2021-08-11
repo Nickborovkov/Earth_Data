a3335a8031703efa0b0682db8e68cb9c961096d2
Nikita Borovkov <borovckov@yandex.ru>
2021-08-12 01:11:58 +0300

    update todo.js

--- src/helpers/todo.js
+++ src/helpers/todo.js
@@ -1,10 +1,8 @@
 //TODO `add search queries`
 //TODO `restyle search results`
 //TODO `Add videos and audios in nasaLibrary after search`
-//TODO `make responsible`
 //TODO `fix timezone bug`
 //TODO `in neows date show not correctly`
 //TODO `try to remove hashRouter with browserRouter`
 //TODO `add limits to forms or at least write limits in forms`
-//TODO `add errors from server to forms`
 //TODO `add errors handling (try/catch)`
