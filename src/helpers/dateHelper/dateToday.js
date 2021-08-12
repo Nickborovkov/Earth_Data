3f6264972946d33ff647270a190df6bb5db9ac41
Nikita Borovkov <borovckov@yandex.ru>
2021-08-13 00:53:44 +0300

    timezone fix

--- src/helpers/dateHelper/dateToday.js
+++ src/helpers/dateHelper/dateToday.js
@@ -4,8 +4,22 @@ export const yearNow = String(now.getFullYear())
 export const monthNow = String(now.getMonth() + 1).length === 1
     ? `0${String(now.getMonth() + 1)}`
     : String(now.getMonth() + 1)
-export const dayNow = String(now.getDate() - 1).length === 1
-    ? `0${String(now.getDate() - 1)}`
-    : String(now.getDate() - 1)
 
-export const dateToday = `${yearNow}-${monthNow}-${dayNow}`
+//Fix for some server requests which work only on timezone UTC-3
+export const dayFixedNow = () => {
+    const hoursNow = now.getHours()
+    const hoursUTCNow = hoursNow - 6
+    let dayNow
+    if(hoursNow - hoursUTCNow < 6 && hoursNow - hoursUTCNow > -6){
+        dayNow = String(now.getDate()).length === 1
+            ? `0${String(now.getDate() )}`
+            : String(now.getDate())
+    }else {
+        dayNow = String(now.getDate() - 1).length === 1
+            ? `0${String(now.getDate() - 1)}`
+            : String(now.getDate() - 1)
+    }
+    return dayNow
+}
+
+export const dateToday = `${yearNow}-${monthNow}-${dayFixedNow()}`
