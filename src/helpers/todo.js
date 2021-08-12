90667f21246aa27588d224a49c242c82246017fb
Nikita Borovkov <borovckov@yandex.ru>
2021-08-13 01:03:51 +0300

    update todo.js

--- src/helpers/todo.js
+++ src/helpers/todo.js
@@ -1,8 +1,10 @@
 //TODO `add search queries`
 //TODO `restyle search results`
 //TODO `Add videos and audios in nasaLibrary after search`
-//TODO `fix timezone bug`
+//TODO `check timezone bug`
 //TODO `in neows date show not correctly`
 //TODO `try to remove hashRouter with browserRouter`
 //TODO `add home icon`
 //TODO `Preloader to photo while it's loading (Jesus...)`
+//TODO `ERRORS HANDLING`
+//TODO `CHANGE MARS ROVER FORM`
