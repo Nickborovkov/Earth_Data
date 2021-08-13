618361f98db4eaa75e6f48a80738be1c9939dc26
Nickborovkov <borovckov@yandex.ru>
2021-08-13 17:08:09 +0300

    update todo.js

--- src/helpers/todo.js
+++ src/helpers/todo.js
@@ -1,10 +1,25 @@
 //TODO `add search queries`
 //TODO `restyle search results`
 //TODO `Add videos and audios in nasaLibrary after search`
-//TODO `check timezone bug`
+
 //TODO `in neows date show not correctly`
+
 //TODO `try to remove hashRouter with browserRouter`
-//TODO `add home icon`
+
 //TODO `Preloader to photo while it's loading (Jesus...)`
-//TODO `ERRORS HANDLING`
-//TODO `CHANGE MARS ROVER FORM`
+
+//TODO `Errors handling`
+//TODO `Empty results handling`
+
+//TODO `mars Rover restyle form`
+
+//TODO `Zooming photos`
+
+//TODO `2 ways of showing result array (list or icons)`
+
+//TODO `Redirecting from nasaLibrary bug`
+
+//TODO `Optimisation (useMemo and other)`
+
+
+//TODO `text fix`
