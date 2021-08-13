504f827477053704bc79a3cf89567739598dea40
Nickborovkov <borovckov@yandex.ru>
2021-08-13 14:25:57 +0300

    added home navLink with icon to header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,6 +4,8 @@ import m from './headerMedia.module.css'
 import cn from 'classnames'
 import SearchForm from "./searchForm/searchForm";
 import mainIcon from '../../images/nasaLogo.png'
+import {NavLink} from "react-router-dom";
+import {RiHome2Line} from "react-icons/ri";
 
 const Header = () => {
     return (
@@ -11,6 +13,8 @@ const Header = () => {
             <div className={cn(s.headerInner, m.headerInner)}>
                 <img className={cn(s.logo, m.logo)} src={mainIcon} alt="mainIcon"/>
                 <h1 className ={cn(s.title, m.title)}>Earth Data</h1>
+                <NavLink className={cn(s.menuButton, m.menuButton)}
+                         to='/'><RiHome2Line/></NavLink>
             </div>
             <SearchForm />
         </header>
