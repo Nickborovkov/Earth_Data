e1a824cd3ca20649b4fe983cbba14f18fb9ebf1e
Nikita Borovkov <borovckov@yandex.ru>
2021-08-15 23:20:07 +0300

    Updates

---
+++ static/js/0.5bef9fd1.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpEarth_Data=this.webpackJsonpEarth_Data||[]).push([[0],{252:function(t,a,n){"use strict";n.d(a,"a",(function(){return c})),n.d(a,"b",(function(){return i}));var r=n(0);function c(t){return Object(r.a)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"}}]})(t)}function i(t){return Object(r.a)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"}}]})(t)}}}]);
+//# sourceMappingURL=0.5bef9fd1.chunk.js.map
