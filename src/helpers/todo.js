a9d8097d44623effc03a0ddfce34e6499b6108d7
Nickborovkov <borovckov@yandex.ru>
2021-08-16 17:25:04 +0300

    update todo.js

--- src/helpers/todo.js
+++ src/helpers/todo.js
@@ -2,20 +2,11 @@
 //TODO `restyle search results+++`
 //TODO `Add videos and audios in nasaLibrary after search+++`
 
-//TODO `in neows date show not correctly---`
-
 //TODO `try to remove hashRouter with browserRouter+++`
 
-//TODO `Preloader to photo while it's loading (Jesus...)--- (maybe with promises)`
-
-//TODO `Zooming photos (Modal icons for images)---`
 
 //TODO `2 ways of showing result array (list or icons)+++`
 
-//TODO `Redirecting from nasaLibrary bug???`
+//TODO `Fix modal windows css bug, add modal windows for forms`
 
 //TODO `Optimisation (useMemo and other)+++`
-
-//TODO `Separate components instead of map+++`
-
-//TODO `Lazy loading for images`
